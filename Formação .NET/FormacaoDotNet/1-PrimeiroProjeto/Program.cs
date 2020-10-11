0dc77a98fea6345ca2f4f67c7cb35b73379dba5d
muriloviscondi <muriloviscondi@gmail.com>
2020-10-11 14:34:35 -0300

    primeiro projeto c sharp

---
+++ Formação .NET/FormacaoDotNet/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Project in Visual Studio!");
+
+            Console.ReadLine();
+        }
+    }
+}
