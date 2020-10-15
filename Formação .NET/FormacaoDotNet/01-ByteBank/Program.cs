721ddad77c4d6f5bd8e251e7afb8593911bd5db1
muriloviscondi <muriloviscondi@gmail.com>
2020-10-14 23:38:37 -0300

    Iniciando parte 2

---
+++ Formação .NET/FormacaoDotNet/01-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaGabriela =  new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 826;
+            contaGabriela.numero = 826549;
+            contaGabriela.saldo = 100;
+
+            Console.ReadLine();
+        }
+    }
+}
