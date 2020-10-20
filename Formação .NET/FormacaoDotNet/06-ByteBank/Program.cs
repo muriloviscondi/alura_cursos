3aa2218251a062d4c431d2365d03c1a23492666e
muriloviscondi <muriloviscondi@gmail.com>
2020-10-20 07:11:17 -0300

    Finalizando modulo 05

---
+++ Formação .NET/FormacaoDotNet/06-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Murilo";
+            cliente.CPF = "123.456.789-00";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
