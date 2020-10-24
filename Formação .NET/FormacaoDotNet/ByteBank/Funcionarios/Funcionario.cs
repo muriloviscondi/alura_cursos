3969ac5233f0875608a4b0b040aebb199d8ff32e
muriloviscondi <muriloviscondi@gmail.com>
2020-10-24 07:16:19 -0300

    Modulo 01 - C# parte 3

---
+++ Formação .NET/FormacaoDotNet/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionário padrão
+        // 1 - diretor
+        // 2 - design
+        // 3 - Gerente de conta corrente
+        // 3 - Coordenador
+        // N - ...
+        private int _tipo;
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+
+        public string Nome { get; set; }
+        
+        public string CPF { get; set; }
+        
+        public double Salario { get; set; }
+
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ Formação .NET/FormacaoDotNet/ByteBank/Program.cs
@@ -0,0 +1,37 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario(1);
+            carlos.Nome = "Carlos";
+            carlos.CPF = "000.000.000.-00";
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "000.000.000.-01";
+            roberta.Salario = 5000;
+            
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+           
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine($"Total de bonificações: {gerenciador.GetTotalBonificacao()}");
+
+            Console.ReadLine();
+        }
+    }
+}
