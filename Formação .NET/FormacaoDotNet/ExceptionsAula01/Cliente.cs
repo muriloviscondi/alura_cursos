633b8ea99c2c1aa22fa734e3303c32e437f28ec7
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2020-12-14 22:41:05 -0300

    estudos alura

---
+++ Formação .NET/FormacaoDotNet/ExceptionsAula01/Cliente.cs
@@ -0,0 +1,24 @@
+﻿namespace ExceptionsAula01
+{
+    public class Cliente
+    {
+        private string _cpf;
+
+        public string Nome { get; set; }
+
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                // Escrevo minha lógica de validação de CPF
+                _cpf = value;
+            }
+        }
+
+        public string Profissao { get; set; }
+    }
+}
