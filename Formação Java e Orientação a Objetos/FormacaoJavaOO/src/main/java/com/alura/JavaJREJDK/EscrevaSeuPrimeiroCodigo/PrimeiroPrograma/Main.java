ca914a8a28a746708d8990111afe2de3d3ccd7af
Murilo Viscondi <50927186+muriloviscondi@users.noreply.github.com>
2022-09-28 21:04:37 -0300

    Add: finalizando capitulo exceptions

--- Formação Java e Orientação a Objetos/FormacaoJavaOO/src/main/java/com/alura/JavaJREJDK/EscrevaSeuPrimeiroCodigo/PrimeiroPrograma/Main.java
+++
@@ -1,7 +0,0 @@
-package com.alura.JavaJREJDK.EscrevaSeuPrimeiroCodigo.PrimeiroPrograma;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
